ad2f0a5370f286cd3ddcf36adc59991d73a4c2db
Gerrit Giehl <gerrit.giehl@email-versand.net>
2016-03-08 14:31:39 +0100

    removed redundant 'sort -u'

--- block-ip-addresses.sh
+++ block-ip-addresses.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 for f in *.txt; do
-    cat $f | sort -u | xargs -I '{}' bash -c 'sudo ip route add blackhole {}'
+    cat $f | xargs -I '{}' bash -c 'sudo ip route add blackhole {}'
 done
 
